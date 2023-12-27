0b1e99b251ade7699397787e2285982bb80b1cef
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2023-12-27 10:12:00 -0400

    searchParams para extraer las query's de la url

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -1,6 +1,13 @@
 import { NextResponse } from 'next/server';
 
 export const GET = async (request, { params }) => {
+  // * searchParams: sirve para extraer las query's de la url
+  const url = new URL(request.url);
+  const { searchParams } = url;
+  // console.log('searchParams: ', searchParams);
+  console.log('get nombre:', searchParams.get('nombre'));
+  console.log('get apellido', searchParams.get('apellido'));
+
   // console.log(params);
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/users/${params.userId}`
